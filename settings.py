21abddd38376a36cef11d3537759263c671f88e6
forget <53308945+forget@users.noreply.github.com>
2022-10-15 01:54:00 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    def __init__(self) -> None:
-        self.token = "" #your access token
-
-        self.delete_after_execute = True #delete command after execution (i.e delete .pfp @user message after it's been executed)
